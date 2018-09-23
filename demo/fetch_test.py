3982827b085f653570a24261a2cdc36349bd948c
mayonglin <mayonglin@lantogis.com>
2018-09-23 15:04:02 +0800

    tmp udpate test

--- demo/fetch_test.py
+++ demo/fetch_test.py
@@ -1,7 +1,17 @@
 import urllib.request
+import re
+
+def getcontent(url, page):
+	headers = ("")
+	opener = urllib.request.build_opener()
+	opener.addheaders = [headers]
+
+	
 
 
 def test_http_fetch():
+
+
 	pass
 
 def main():
