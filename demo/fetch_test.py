a8f9e1399c4fa5614ea3cff637df0fdbd6147e4b
mayonglin <mayonglin@lantogis.com>
2018-10-08 12:18:13 +0800

    update test

--- demo/fetch_test.py
+++ demo/fetch_test.py
@@ -2,7 +2,7 @@ import urllib.request
 import re
 
 def getcontent(url, page):
-	headers = ("")
+	headers = ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36")
 	opener = urllib.request.build_opener()
 	opener.addheaders = [headers]
 
